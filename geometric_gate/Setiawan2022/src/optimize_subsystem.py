1ae41b3cf0abf9e1cfcf8afa17db42c51e34f5bb
brisenodaniel <dbrisenoservin@chapman.edu>
2023-04-03 14:11:01 -0700

    Added Subsytem class, all tests passed

--- geometric_gate/Setiawan2022/src/optimize_subsystem.py
+++ geometric_gate/Setiawan2022/src/optimize_subsystem.py
@@ -14,18 +14,14 @@ File contains methods which:
 
 Custom Datatypes:
     Qobj: Alias for qt.Qobj
-    Qsys: Dictionary with values of type Qobj corresponding to operators acting on
-     some quantum system. Must include key 'H' with value corresponding to the system
-    #  hamiltonian.
+   
 
 Requirements:
     `constr_qsys` accepts argument `nlev` which determines the number of energy
     levels to consider for the quantum system.
 
-    `constr_qsys` returns a dictionary of type {str:Qobj} of operators defined
-    for the quantum system state vector. Dictionary contains entry with label 'H' 
-    corresponding to the hamitonian of the system. Dictionaries of this type are
-    referred to in this file as datatype `Qsys`.
+    `constr_qsys` returns a Subsystem with hamiltonian operator defined. Subystem\
+    class defined in subsystems module
 """
 
 from typing import TypeAlias, TypeVar, Callable, Any
@@ -33,19 +29,20 @@ import qutip as qt
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
+from subsystems import Subsystem
 
 Qobj: TypeAlias = qt.Qobj
 Qsys: TypeAlias = dict[str,Qobj]
 
 j: complex = complex(0,1)
 
-def stabilize_nlev(constr_qsys:Callable['...',Qsys], 
+def stabilize_nlev(constr_qsys:Callable['...', Subsystem], 
                   constr_args:dict,
-                  stable_levels:int=5,
+                  stable_levels:int=3,
                   tol:float=1e-9,
-                  min_nlev:int=5,
+                  min_nlev:int=None,
                   max_nlev:int=None,
-                  n_stable_reps:int=5)->tuple[Qsys, int]:
+                  n_stable_reps:int=5)->tuple[Subsystem, int]:
     """Determines the minimum number of energy levels to consider to simulate\
     a given quantum system such that the relevant energy levels do not change\
     thier eigenenergies as the Hamiltonian considers higher energy states.
@@ -62,7 +59,7 @@ def stabilize_nlev(constr_qsys:Callable['...',Qsys],
         tol (float, optional): Numerical error tolerance. If two eigenenergies e1 and e2 are such that |e1-e2|<=tol,\
          then e1,e2 will be considered equal.
         min_nlev (int, optional): Minimum number of simulated levels to consider. Must be\
-         greater than `stable_levels`. Defaults to 5.
+         greater than `stable_levels`. If not provided, will be set to the value of `stable levels. Defaults to None.
         max_nlev (int, optional): Maximum number of simulated levels to consider. If not \
          provided, function will run until stabilization is achieved. Defaults to None.
         n_stable_reps (int, optional): Number of additional energy states of the Hamiltionian \
@@ -75,6 +72,8 @@ def stabilize_nlev(constr_qsys:Callable['...',Qsys],
         tuple[Qsys, int]: (qsys, nlev), where nlev is the minimum number of energy levels modeled to achieve \
          stability of the eigenenergies, and qsys is the return value of `constr_qsys` with nlev energy levels.
     """
+    if min_nlev is None:
+        min_nlev = stable_levels
     assert stable_levels <= min_nlev, \
     'Lower bound of simulated levels `min_nlev`:{} is smaller than desired \
         number of stable levels `stable_levels`:{}'.format(min_nlev, stable_levels)
@@ -113,8 +112,8 @@ def stabilize_nlev(constr_qsys:Callable['...',Qsys],
 
 def get_energies_over_nlev(constr_qsys:Callable['...',Qsys],
                            constr_args:dict,
-                           track_bottom_n_levels:int=None,
-                           min_nlev:int=5,
+                           track_bottom_n_levels:int=3,
+                           min_nlev:int=None,
                            max_nlev:int=30)->list[np.ndarray]:
     """Function tracks eigenenergies of a system's hamiltonian as the number\
     of energy levels is varied between [min_nlev,max_nlev].
@@ -126,13 +125,15 @@ def get_energies_over_nlev(constr_qsys:Callable['...',Qsys],
         track_bottom_n_levels (int, optional): For a given value n, function will track the eigenenergies of the bottom\
          n energy levels. If not provided, all eigenenergies of the given hamiltonian will be returned. Defaults to None.
         min_nlev (int, optional): Minimum number of simulated levels to consider. Must be\
-         greater than `stable_levels`. Defaults to 5.
+         greater than `stable_levels`. If not provided, will be set to the value of `track_bottom_n_levels`.
         max_nlev (int, optional): Maximum number of simulated levels to consider. Defaults to 30
 
     Returns:
         list[np.ndarray]: 2d iterable with indices [nlev-1][eigenen], where nlev is the number of modeled energy\
          levels and eigenen is the list of eigenenergies for hamiltonian eigenstates as set by `track_bottom_n_levels`
     """
+    if min_nlev is None:
+        min_nlev = track_bottom_n_levels
     assert track_bottom_n_levels<=min_nlev, \
     'Lower bound of simulated levels `min_nlev`:{} is smaller than desired \
         number of tracked levels:{}'.format(min_nlev, track_bottom_n_levels)
@@ -149,8 +150,8 @@ def get_energies_over_nlev(constr_qsys:Callable['...',Qsys],
 
 def plot_energies_over_nlev(constr_qsys:Callable['...', Qsys],
                             constr_args:dict,
-                            track_bottom_n_levels:int=5,
-                            min_nlev:int=5,
+                            track_bottom_n_levels:int=3,
+                            min_nlev:int=None,
                             max_nlev:int=30,
                             print_to:str=None)->None:
     """Function plots eigenenergies of a system's hamiltonian as the number\
@@ -162,7 +163,7 @@ def plot_energies_over_nlev(constr_qsys:Callable['...', Qsys],
         track_bottom_n_levels (int, optional): For a given value n, function will plot the eigenenergies of the bottom\
              n energy levels. Defaults to 5.
         min_nlev (int, optional): Minimum number of simulated levels to consider. Must be\
-         greater than `stable_levels`. Defaults to 5.
+         greater than `stable_levels`. If not provided, will be set to the value of `track_bottom_n_levels`.
         max_nlev (int, optional): Maximum number of simulated levels to consider. Defaults to 30\
         print_to (str, optional): File name of eigenenergy plot. File will be placed in `../plots` directory.\
          If not provided, function will print plot to standard out.
@@ -170,6 +171,8 @@ def plot_energies_over_nlev(constr_qsys:Callable['...', Qsys],
     Returns:
        None
     """
+    if min_nlev is None:
+        min_nlev = track_bottom_n_levels
     params:dict = locals() # make copy of function parameters
     del params['print_to']
     eigenen:list[np.ndarray] = get_energies_over_nlev(**params)
@@ -196,7 +199,7 @@ def is_diag(op:Qobj, tol:float=1e-9)->bool:
 
     Args:
         op (Qobj): Quantum operator
-         tol (int, optional): Numerical error tolerance. If \
+        tol (int, optional): Numerical error tolerance. If \
          a matrix element has value with magnitude less than tol,\
          element will be considered 0.
 
@@ -205,13 +208,11 @@ def is_diag(op:Qobj, tol:float=1e-9)->bool:
     """
     diags = op.diag()
     diag_op = qt.qdiags(diags,0)
-    diff_op = abs(op.full()-diag_op.full())
-    
-    return (diff_op<tol).all()
+    return qt.isequal(diag_op, op, tol)
 
 def has_equal_energies(qsys_1:Qsys, 
                        qsys_2:Qsys,
-                       stable_levels:int,
+                       stable_levels:int=None,
                        tol:float=1e-9) -> bool:
     """Function determines if two Qsys have hamiltonians with\
       equal eigenenergies for bottom `stable_levels` eigenstates.
@@ -229,6 +230,8 @@ def has_equal_energies(qsys_1:Qsys,
         bool: True if first two parameters have identical eigenenergies for bottom `stable_levels`\
          eigenstates.
     """
+    if stable_levels is None:
+        stable_levels = qsys_1['H'].dims[0][0]
     H1:Qobj = qsys_1['H']
     H2:Qobj = qsys_2['H']
     e1:np.ndarray = H1.eigenenergies()[:stable_levels]
@@ -238,76 +241,92 @@ def has_equal_energies(qsys_1:Qsys,
     else:
         return all(abs(e1-e2)<=tol)
 
-def diagonalize_Qsys(qsys:Qsys)->tuple[Qsys,np.ndarray[Qobj]]:
+def diagonalize_Qsys(qsys:Subsystem)->Subsystem:
     """Function finds the eigenbasis for the hamiltonian in qsys, and \
     transforms all operators in qsys to that eigenbasis
 
     Args:
-        qsys (Qsys): Quantum system with operators to be transformed to its hamiltonian's\
+        qsys (Subsystem): Quantum system with operators to be transformed to its hamiltonian's\
          eigenbasis.
 
     Returns:
-        tuple[Qsys,np.ndarray[Qobj]]: (sys, basis) where sys is qsys transformed to \
-         it's hamiltonian's eigenbasis and basis is the eigenbasis.
+        Subsystem: Subsystem identical to `qsys` but in hamiltonian eigenbasis.
     """
     if is_diag(qsys['H']):
-        return qsys, qsys['H'].eigenstates()[1]
+        return qsys
     _, eigenbasis = qsys['H'].eigenstates()
-    for key, operator in qsys.items():
-        qsys[key] = operator.transform(eigenbasis)
-    return qsys, eigenbasis
+    diag_sys = qsys.transform(eigenbasis)
 
-def truncate_Qsys(qsys:Qsys,
-                  nlev:int,
-                  eigenbasis:np.ndarray[Qobj]=None)->tuple[Qsys,np.ndarray[Qobj]]:
+    return diag_sys
+
+def truncate_Qsys(qsys:Subsystem,
+                  truncate_to:int)->Subsystem:
     """Function truncates all operators in qsys to an operator acting on a state with `nlev`
     energy levels. Truncates basis vectors in `eigenbasis` if provided.
 
     Args:
-        qsys (Qsys): Quantum system with operators to be truncated.
-        nlev (int): Number of energy levels to truncate to.
+        qsys (Subsystem): Quantum system with operators to be truncated.
+        truncate_to (int): Number of energy levels to truncate to.
         eigenbasis (np.ndarray[Qobj], optional): If provided, eigenbasis of qsys relative to some \
             basis. If not provided, will be set to the eigenstates of the hamiltonian in qsys.\
             Defaults to None.
 
     Returns:
-        tuple[Qsys,np.ndarray[Qobj]]: (sys, basis) where sys is `qsys` with all Qobj truncated to `nlev`\
+        tuple[Subsystem,np.ndarray[Qobj]]: (sys, basis) where sys is `qsys` with all Qobj truncated to `truncate_to`\
          energy levels, and basis is `eigenbasis` identically truncated.
     """
-    for key, operator in qsys.items():
-        qsys[key] = operator.extract_states(range(nlev))
-    if eigenbasis is not None:
-        for idx, eigenstate in enumerate(eigenbasis):
-            eigenbasis[idx] = eigenstate.extract_states(range(nlev))
-    else:
-        _, eigenbasis = qsys['H'].eigenstates()
-    return qsys, eigenbasis
+    return qsys.truncate(truncate_to)
+    # # check dimensions of qsys are greater than or equal to desired truncation length
+    # for key, operator in qsys.items():
+    #     nlev = operator.full().shape[0]
+    #     assert truncate_to<=nlev, \
+    #     f'Desired truncation length {truncate_to} is greater than number of energy levels in operator'\
+    #     +f' {key}:{nlev}.'
+    # # check dimensions of the basis are greater than or equal to desired truncation length
+    # assert truncate_to<=len(eigenbasis),\
+    # f'Desired truncation length {truncate_to} indicates a vector space of higher dimension than'\
+    # + f' number of basis vectors in `eigenbasis`:{len(eigenbasis)}'
+    # for eigenstate in eigenbasis:
+    #     nlev = eigenstate.full().shape[0]
+    #     assert truncate_to<=nlev,\
+    #     f'Desired truncation length {truncate_to} is greater than length of basis vectors {nlev}'
+    # # truncate operators
+    # for key, operator in qsys.items():
+    #     qsys[key] = operator.extract_states(range(truncate_to))
+    # # truncate eigenbasis
+    # if eigenbasis is not None:
+    #     eigenbasis = eigenbasis[:truncate_to]
+    #     for idx, eigenstate in enumerate(eigenbasis):
+    #         eigenbasis[idx] = eigenstate.extract_states(range(truncate_to))
+    # else:
+    #     _, eigenbasis = qsys['H'].eigenstates()
+    # return qsys, eigenbasis
 
-def build_optimized_system(constr_qsys:Callable['...', Qsys],
+def build_optimized_system(constr_qsys:Callable['...', Subsystem],
                            constr_args:dict,
-                           stable_levels:int=5,
+                           stable_levels:int=3,
                            tol:float=1e-9,
-                           min_nlev:int=5,
+                           min_nlev:int=None,
                            max_nlev:int=None,
                            truncate_to:int=None,
                            n_stable_reps:int=5
-                           )->tuple[Qsys,np.ndarray[Qobj]]:
+                           )->tuple[Subsystem,np.ndarray[Qobj]]:
     """Function first builds the quantum system defined by `constr_qsys` with enough energy levels\
     to model dynamics of the bottom `stable_levels` eigenstates (ordered by increasing eigenenergy)\
     with minimal truncation error. Then, the stable system model is transformed into it's hamiltonian's\
     eigenbasis, and truncated to `truncate_to` energy levels.
 
     Args:
-        constr_qsys (function): Constructor which returns dictionary of quantum operators (Qsys)\
+        constr_qsys (function): Constructor which returns dictionary of quantum operators (Subsystem)\
          corresponding to operators acting on a quantum system, including the Hamiltonian which \
          must have key 'H'. Must accept a parameter `nlev` which determines the number of energy \
-         levels modeled in Qsys.
+         levels modeled in Subsystem.
         constr_args (dict): Named arguments to constr_qsys. Need not include `nlev`.
         stable_levels (int, optional): Determines which energy levels are stabilized. 
          For a given value n, [0,1,2,...,n-1] levels will be stabilized. Defaults\
          to 5.    
          min_nlev (int, optional): Minimum number of simulated levels to consider. Must be\
-         greater than `stable_levels`. Defaults to 5.
+         greater than or equal to `stable_levels`. If not provided, will be set to the value of `stable levels`.
         max_nlev (int, optional): Maximum number of simulated levels to consider. If not \
          provided, function will run until stabilization is achieved. Defaults to None.
         truncate_to (int, optional): Number of energy levels to truncate to after all operators\
@@ -320,20 +339,27 @@ def build_optimized_system(constr_qsys:Callable['...', Qsys],
          with n eigenstates. Defaults to 5.
 
     Returns:
-        tuple[Qsys,np.array[Qobj]]: tuple(sys, basis) where sys is the quantum system returned by `constr_sys`\
+        tuple[Subsystem,np.array[Qobj]]: tuple(sys, basis) where sys is the quantum system returned by `constr_sys`\
          transformed to it's hamiltonian's eigenbasis and truncated to `truncate_to` energy levels with minimal\
          truncation error; and `basis` is the truncated hamiltonian eigenbasis. `basis` is given relative to the\
          basis in which `constr_sys` defines the system operators.
     """
     if truncate_to is None:
         truncate_to = stable_levels
+    if min_nlev is None:
+        min_nlev = stable_levels
+    assert truncate_to<=min_nlev,\
+    f'Parameter `truncate_to` must have value less than or equal to `min_nlev`.'+\
+    f' `truncate_to` has value {truncate_to}, `min_nlev` has {min_nlev}.' +\
+    f' If you did not provide `min_nlev` as a parameter, it was set to the value of parameter `stable_levels`.'
     qsys, _ = stabilize_nlev(constr_qsys,
-                            constr_args,
-                            stable_levels,
-                            tol,
-                            min_nlev,
-                            max_nlev,
-                            n_stable_reps)
-    qsys, eigenbasis = diagonalize_Qsys(qsys)
-    qsys, eigenbasis = truncate_Qsys(qsys, truncate_to, eigenbasis)
-    return qsys, eigenbasis
+                             constr_args,
+                             stable_levels,
+                             tol,
+                             min_nlev,
+                             max_nlev,
+                             n_stable_reps)
+    qsys = diagonalize_Qsys(qsys)
+    qsys = qsys.truncate(truncate_to)
+   # qsys, eigenbasis = truncate_Qsys(qsys, truncate_to, eigenbasis)
+    return qsys
--- geometric_gate/Setiawan2022/src/subsystems.py
+++ geometric_gate/Setiawan2022/src/subsystems.py
@@ -7,18 +7,233 @@ in the literature folder.
 
 All operators are defined in the Quantum Harmonic Oscillator basis.
 """
+from __future__ import annotations
 from typing import TypeAlias
+from dataclasses import dataclass 
+from _collections_abc import Sequence
+import warnings
 import qutip as qt
 import numpy as np
 
 Qobj: TypeAlias = qt.Qobj
 j: complex = complex(0,1)
 
+@dataclass
+class Subsystem:
+    """Container class for operators defining a quantum system. Class can be\
+    accessed as if it were a dictionary of operators with string labels. as:
+
+            `Subsystem[str_label] = oper`, to add `oper` to `Subsystem`, or
+            `oper = Subsystem[str_label]`, to retrieve `oper` from `Subystem`
+
+    If the hamiltonian operator with label 'H' is not defined in the class, a \
+    warning will be thrown when trying to access its operators.
+
+    A Subsystem can be initialized by providing a dictionary of qt.Qobj operators\
+    as qsys = Subystem({lbl:Qobj})
+
+    Public attributes:
+        base_space (str): A label indicating in which basis the operators in the class \
+         are currently defined.
+        tol (float): A decimal corresponding to numerical error tolerance used in comparison\
+         operations with other Subsystems. See documentation for qt.isequal().
+    """
+    # _ops:dict[str:Qobj] # Dictionary of quantum operators. Should not be directly accessed. To access, use Subsystem[op_label] instead.
+    # _nlev:int|None = None # This field should never be set manually
+    # _base_space:str = "QHO" # basis in which operators are assumed to be originally defined
+    # _tranf_list:np.ndarray[Qobj]|None = None # List of change-of-basis operators (or bases transformed into) applied to operators in system, in the order they were applied
+    # _tol:int = 1e-9 # numerical error tolerance to be used in comparison operations
+    # _state:Qobj|None = None # State vector of the quantum system.
+
+
+    def __init__(self, ops:dict[str:Qobj], base_space='QHO', tol=1e-9)->Subsystem:
+        self._ops:dict = {} # Dictionary of quantum operators. Should not be directly accessed. To access, use Subsystem[op_label] instead.
+        self._nlev:int|None = None # Number of energy levels considered in system model. This field should never be set manually
+        self._base_space:str = base_space # basis in which operators are assumed to be originally defined
+        self._transf_list:list[Qobj|np.ndarray(Qobj)] = [] # List of change-of-basis operators (or bases transformed into) applied to operators in system, in the order they were applied
+        self._tol:float = tol # numerical error tolerance to be used in comparison operations
+        self._state: Qobj|None = None
+        for key, value in ops.items():
+            self.__setitem__(key,value)
+
+
+    #### getters and setters
+    #for _state
+    @property
+    def state(self)->Qobj:
+        return self._state
+    
+    @state.setter
+    def state(self, vect:int|Qobj)->None:
+        assert isinstance(vect,'Qobj'), \
+        f'State vector must be a Qobj ket, type {type(vect)} given'
+        assert vect.isket, 'State vector must be a ket'
+        vect_dims = vect.dims[0][0]
+        assert vect_dims == self._nlev, \
+        f'State vector must have {self._nlev} coordinates, but has {vect_dims}'
+        self._state = vect
+
+    # for _base_space
+    @property
+    def base_space(self)->str:
+        return self._base_space
+    
+    @base_space.setter
+    def base_space(self, basis_lbl:str)->None:
+        self._base_space = basis_lbl
+
+    # for _tol
+    @property
+    def tol(self)->float:
+        return self._tol
+    
+    @tol.setter
+    def tol(self, tolerance)->None:
+        assert isinstance(tolerance, float),\
+            "Cannot add non-float type as tol"
+        self._tol = tolerance
+
+
+    def __eq__(self, other:Subsystem)->bool:
+        """Comparison operator for subsystems.
+        WARNING: If _tol operator is not identical for both subsystems,\
+            equality comparisons will may not be symmetric. That is , sys1==sys2 may\
+            give different result than sys2==sys1. If _tol is not equal across both\
+            subsystems, function will throw a warning.
+
+        Args:
+            other (Subsystem): other subsystem to compare
+
+        Returns:
+            bool: True if all fields of both subsytems are equal
+        """
+        if self._tol!=other._tol:
+            warnings.warn(
+                "Numerical error tolerance parameter _tol has been set to"\
+                + " different values for the two subsytems. Equality comparison"\
+                + f" may not be symmetric. For left system _tol={self._tol}."\
+                + f" For second system _tol={other._tol}"
+            )
+        if self._nlev!=other._nlev: return False
+        if self._nlev is not None: # self._nlev==None indicates empty subsysem
+            # from here on out we can safely assume both subsystems are populated,
+            # and their operators and basis are of equal dimension
+            for key in self._ops: #compare operators
+                if key not in other._ops: return False
+                if not qt.isequal(self._ops[key], other._ops[key], self._tol): return False
+            if self._base_space != other._base_space: return False
+            #check basis, last thing to check
+            if self._basis is None: #if no basis defined, check that other also has no basis defined
+                if other._basis is not None: return False
+            else:
+                if other._basis is None: return False
+                for idx, vector in enumerate(self._basis):
+                    other_vector = other._basis[idx]
+                    if not qt.isequal(vector, other_vector, self._tol): return False
+        return True
+    
+    def __getitem__(self, key:str)->Qobj:
+        assert key in self._ops, f"Operator {key} not defined for subsystem."
+        if 'H' not in self._ops:
+            warnings.warn(
+                'Accessing operator from subsystem with no hamiltonian. Subsystem is ill-defined.'
+                )
+        return self._ops[key]
+    
+    def __setitem__(self, key:str, value:Qobj)->None:
+        assert value.isoper, \
+            f"Only Qobj operators may be added via hashing. No bras or kets."
+        op_dims = value.dims[0][0]
+        if self._nlev is None:
+            self._nlev = op_dims
+        assert self._nlev==op_dims,\
+            f"Cannot add operator of dimension {op_dims}"\
+            + f" to system of dimension {self.nlev}."
+        self._ops[key] = value
+
+    def __contains__(self, key:str)->bool:
+        return key in self._ops
+    
+    def __iter__(self):
+        return self._ops.__iter__()
+
+    def items(self)-> dict[str:Qobj]:
+        return self._ops.items()
+    
+    #### Non-dict Methods ###
+    def transform(self, transf:Sequence) -> Subsystem:
+        """Transform into basis indicated by `transf`
+
+        Args:
+            transf (Sequence): Array-like object specifying either a transfomration matrix or a list\
+                of kets which make up the new basis for the operators in ops.
+            
+
+        Returns:
+            Subsystem: Subsystem identical to `self`, but in the basis indicated by `transf`.
+        """
+        new_ops:dict[str:Qobj] = {}
+        new_transf_list = self._transf_list + [transf]
+        new_state:Qobj = None
+        if self._state is not None:
+            new_state = self._state.transform(transf)
+        for key, op in self._ops.items():
+            new_ops[key] = op.transform(transf)
+
+        new_sys = Subsystem(new_ops)
+        new_sys._state = new_state
+        new_sys._transf_list = new_transf_list
+        new_sys._base_space = self._base_space
+        new_sys._tol = self._tol
+        return new_sys
+    
+    def truncate(self, truncate_to:int)->Subsystem:
+        """Generates a Subsystem identical to `self`, but truncated to `nlev`\
+        energy levels.
+
+        Args:
+            truncate_to (int): Number of energy levels to truncate to
+        Returns:
+            Subsystem: Subsystem identical to `self`, but truncated to `nlev`\
+            energy levels.
+        """
+        assert truncate_to<=self._nlev,\
+        f'Desired truncation length {truncate_to} specifies a system with higher'\
+        f' number of energy levels than self: {self._nlev}'
+        if truncate_to == self._nlev:
+            return self
+        new_ops:dict[str:Qobj] = {}
+        new_transf_list = []
+        new_state = None
+        keep_states = list(range(truncate_to))
+        if self._state is not None:
+            new_state = new_state.extract_states(keep_states)
+        for key, op in self._ops.items():
+            new_ops[key] = op.extract_states(keep_states)
+        for transf in self._transf_list:
+            if isinstance(transf, Qobj):
+                new_transf_list += [transf.extract_states(keep_states)]
+            else:
+                trunc_basis = np.empty(len(transf), dtype=object)
+                for i, basis_vect in enumerate(transf):
+                    trunc_basis[i] = basis_vect.extract_states(keep_states)
+                new_transf_list += [trunc_basis]
+        new_sys = Subsystem(new_ops)
+        new_sys._transf_list = new_transf_list
+        new_sys._state = new_state
+        new_sys._base_space = self._base_space
+        new_sys._tol = self._tol 
+        return new_sys
+
+        
+
+    
+
 def build_fluxonium_operators(nlev:int,
                               E_C:float,
                               E_J:float,
                               E_L:float,
-                              phi_ext:float) -> dict[str,Qobj]:
+                              phi_ext:float) -> Subsystem:
     """Method defines operators for a fluxonium circuit as defined in
     Setiawan et. al. 2022.
 
@@ -40,7 +255,9 @@ def build_fluxonium_operators(nlev:int,
     n:Qobj = n_zpf*(a + a.dag())
     phi:Qobj = phi_zpf*j*(a-a.dag())
     H:Qobj = build_fluxonium_hamiltonian(n,phi,E_C, E_J, E_L, phi_ext)
-    return {'n':n, 'phi':phi, 'H': H}
+    ops = {'n':n, 'phi':phi, 'H':H}
+    fluxonium = Subsystem(ops)
+    return fluxonium
 
 def build_fluxonium_hamiltonian(n:Qobj, 
                                 phi:Qobj, 
@@ -72,7 +289,7 @@ def build_fluxonium_hamiltonian(n:Qobj,
     H = capacitor_energy + josephenson_energy + inductor_energy
     return H
 
-def build_transmon_operators(nlev:int, w:float, U:float) -> dict[str,Qobj]:
+def build_transmon_operators(nlev:int, w:float, U:float) -> Subsystem:
     """Method defines the hamiltonian operator for a transmon circuit as described
     in Setiawan et. al. 2022
 
@@ -87,4 +304,6 @@ def build_transmon_operators(nlev:int, w:float, U:float) -> dict[str,Qobj]:
     """
     a:Qobj = qt.destroy(nlev)
     H:Qobj = w*a.dag()*a - U*a.dag()**2 * a**2
-    return {'a': a, 'H': H}
+    ops = {'a':a, 'H':H}
+    transmon = Subsystem(ops)
+    return transmon
